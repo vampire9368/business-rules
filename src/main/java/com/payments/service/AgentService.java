76eeb6faf5e0b038edba8e9db4a0c12d6243ca7a
vampire9368 <ajaykumarbathula2019@gmail.com>
2020-08-21 10:43:06 +0530

    New models and services

--- src/main/java/com/payments/service/AgentService.java
+++ src/main/java/com/payments/service/AgentService.java
@@ -1,5 +1,12 @@
 package com.payments.service;
 
+import com.payments.model.Agent;
+import com.payments.model.Payment;
+
 public class AgentService {
 
+	public Float addCommisionToAgent(Payment payment , Agent agent) {
+		return null;
+		
+	}
 }
--- src/main/java/com/payments/service/PackagingService.java
+++ src/main/java/com/payments/service/PackagingService.java
@@ -1,5 +1,10 @@
 package com.payments.service;
 
+import com.payments.model.Customer;
+
 public class PackagingService {
 
+	public String generatePackingSlip(Customer customer) {
+		return "New Packing slip";
+	}
 }
